3d2d80e1ebac0bba2f9a73d7ed978053280a6b71
krepak <krepakvitaliy@gmail.com>
2021-05-11 15:02:33 +0300

    add pr_info

--- XDMA/linux-kernel/xdma/device.c
+++ XDMA/linux-kernel/xdma/device.c
@@ -509,8 +509,7 @@ static void submit_noinput_sg_buffer(struct vcam_out_buffer *buf,
 		vbuf_sgt, vbuf_sgt->sgl->length, xc, buf, (u64)count, (u64)pos, write,
 		engine->name);
 
-    pr_info("size %d, rowsize %d, rows %d\n",
-		size, rowsize, rows);
+    pr_info("size %d, rowsize %d, rows %d\n", size, rowsize, rows);
 
     rv = xcdev_check(__func__, xc, 1);
 	if (rv < 0){
@@ -787,9 +786,16 @@ int submitter_thread(void *data)
             dev->output_fps.numerator = 1001;
             dev->output_fps.denominator = 1000 * computation_time_ms;
         } else if (timeout > computation_time_jiff) {
+            pr_info("schedule_timeout_interruptible %d \n", timeout - computation_time_jiff);
             schedule_timeout_interruptible(timeout - computation_time_jiff);
         }
-        pr_info("timeout_ms %d, timeout_ms %d \n", timeout_ms, timeout);
+        pr_info("timeout_ms %d, timeout %d \n", timeout_ms, timeout);
+        pr_info("computation_time_jiff %d \n", computation_time_jiff);
+        pr_info("dev->output_fps.numerator %d \n", dev->output_fps.numerator);
+        pr_info("dev->output_fps.denominator %d \n", dev->output_fps.denominator);
+        pr_info("computation_time_ms %d \n", computation_time_ms);
+        pr_info("jiffies %d \n", jiffies);
+        pr_info("computation_time_jiff %d \n", computation_time_jiff);
     }
 
     return 0;
