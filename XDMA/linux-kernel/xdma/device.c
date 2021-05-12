a0f4f49f38a1653474ebe30c77a46b602e1a8749
krepak <krepakvitaliy@gmail.com>
2021-05-12 15:47:15 +0300

    print sg buf dma size

--- XDMA/linux-kernel/xdma/device.c
+++ XDMA/linux-kernel/xdma/device.c
@@ -506,7 +506,7 @@ static void submit_noinput_sg_buffer(struct vcam_out_buffer *buf,
     size_t rows = dev->output_format.height;
 
 	pr_info("vbuf_sgt 0x%p, vubf len %d, priv 0x%p, vcam_out_buffer 0x%p, %llu, pos %llu, W %d, %s.\n",
-		vbuf_sgt, vbuf_sgt->sgl->length, xc, buf, (u64)count, (u64)pos, write,
+		vbuf_sgt, sg_dma_len(vbuf_sgt->sgl), xc, buf, (u64)count, (u64)pos, write,
 		engine->name);
 
     pr_info("size %d, rowsize %d, rows %d\n", size, rowsize, rows);
