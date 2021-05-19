9407b2988d8adaeb6ab997cfcae9c9d968a3e6c6
Виталий Крепак <krepak@dc.inviewlab.com>
2021-05-19 10:13:05 +0000

    change scripts

--- XDMA/linux-kernel/tests/load_driver.sh
+++ XDMA/linux-kernel/tests/load_driver.sh
@@ -30,7 +30,7 @@ modprobe videobuf-vmalloc
 # Use the following command to Load the driver in the default 
 # or interrupt drive mode. This will allow the driver to use 
 # interrupts to signal when DMA transfers are completed.
-insmod ../xdma/xdma.ko 
+insmod ../xdma/xdma.ko poll_mode=1
 # Use the following command to Load the driver in Polling
 # mode rather than than interrupt mode. This will allow the
 # driver to use polling to determ when DMA transfers are 
@@ -57,9 +57,25 @@ else
   exit 1
 fi
 
-#../tools/reg_rw /dev/xdma0_user 0x80 -w 0x00
-#../tools/reg_rw /dev/xdma0_user 0x84 -w 0x00
+
+../tools/reg_rw /dev/xdma0_user 0x20 -w 0x1e0
+../tools/reg_rw /dev/xdma0_user 0x30 -w 0x1e0
+../tools/reg_rw /dev/xdma0_user 0x40 -w 0x32dcd5
+../tools/reg_rw /dev/xdma0_user 0x50 -w 0x05
+
+../tools/reg_rw /dev/xdma0_user 0x60 -w 0x01
+
+../tools/reg_rw /dev/xdma0_user 0x80 -w 0x01
+
+../tools/reg_rw /dev/xdma0_user 0x10 -w 0x00
+../tools/reg_rw /dev/xdma0_user 0x80 -w 0x01
 sleep 1
+../tools/reg_rw /dev/xdma0_user 0x10 -w 0x01
+../tools/reg_rw /dev/xdma0_user 0x80 -w 0x01
+
+exit 0
+
+
 
 ../tools/reg_rw /dev/xdma0_user 0x10 -w 0x01
 
--- XDMA/linux-kernel/tools/setup_tpg.sh
+++ XDMA/linux-kernel/tools/setup_tpg.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-./reg_rw /dev/xdma0_user 0x20 -w 0x280
-./reg_rw /dev/xdma0_user 0x30 -w 0x100
-./reg_rw /dev/xdma0_user 0x40 -w 0x32dcd5
-./reg_rw /dev/xdma0_user 0x50 -w 0x01
+./reg_rw /dev/xdma0_user 0x20 -w 0x1e0
+./reg_rw /dev/xdma0_user 0x30 -w 0x1e0
+./reg_rw /dev/xdma0_user 0x40 -w 0x196e6a
+./reg_rw /dev/xdma0_user 0x50 -w 0x05
+
 ./reg_rw /dev/xdma0_user 0x60 -w 0x01
 
 ./reg_rw /dev/xdma0_user 0x80 -w 0x01
@@ -15,4 +16,6 @@ sleep 1
 ./reg_rw /dev/xdma0_user 0x10 -w 0x01
 ./reg_rw /dev/xdma0_user 0x80 -w 0x01
 
-./dma_from_device -v -d /dev/xdma0_c2h_0 -f output_data.bin -s 0x000a0000
+
+sudo ./dma_from_device -v -d /dev/xdma0_c2h_0 -f output_data.bin -s 0xE1000
+#./dma_from_device -v -d /dev/xdma0_c2h_0 -f output_data.bin -s 0x000a0000
